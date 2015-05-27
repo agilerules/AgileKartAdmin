b1691c3f53288d2b49808ee785913eac0114d367
shyamnarayan2001 <shyamnarayan2001@gmail.com>
2015-05-28 00:17:02 +0530

    Modified the port number for the rest URL from 8089 to 8080

--- WebContent/scripts/services/AkCategoryOptionsFactory.js
+++ WebContent/scripts/services/AkCategoryOptionsFactory.js
@@ -1,4 +1,4 @@
 angular.module('agileRulesKart').factory('AkCategoryOptionsResource', function($resource){
-    var resource = $resource('http://localhost:8089/AgileKartRest/rest/akcategoryoptions/:AkCategoryOptionsId',{AkCategoryOptionsId:'@categoryOptionId'},{'queryAll':{method:'GET',isArray:true},'query':{method:'GET',isArray:false},'update':{method:'PUT'}});
+    var resource = $resource('http://localhost:8080/AgileKartRest/rest/akcategoryoptions/:AkCategoryOptionsId',{AkCategoryOptionsId:'@categoryOptionId'},{'queryAll':{method:'GET',isArray:true},'query':{method:'GET',isArray:false},'update':{method:'PUT'}});
     return resource;
 });
